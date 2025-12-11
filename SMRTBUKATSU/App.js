48d4c3a3b9b0dcaf78273e721160607d04d91443
inouetakehiko <inoue420@gmail.com>
2025-12-11 23:51:21 +0900

    addVideoとした

--- SMRTBUKATSU/App.js
+++ SMRTBUKATSU/App.js
@@ -5,7 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import VideoListScreen from './src/screens/VideoListScreen';
-import AddvideoScreen from './src/screens/AddvideoScreen'; 
+import AddVideoScreen from './src/screens/AddVideoScreen'; 
 import TaggingScreen from './src/screens/TaggingScreen';
 import HighlightPlayerScreen from './src/screens/HighlightPlayerScreen';
 
@@ -16,7 +16,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="VideoList">
         <Stack.Screen name="VideoList" component={VideoListScreen} options={{ title: '動画一覧' }} />
-        <Stack.Screen name="AddVideo" component={AddvideoScreen} options={{ title: '動画追加' }} />
+        <Stack.Screen name="AddVideo" component={AddVideoScreen} options={{ title: '動画追加' }} />
         <Stack.Screen name="Tagging" component={TaggingScreen} options={{ title: 'タグ付け' }} />
         <Stack.Screen name="Highlights" component={HighlightPlayerScreen} options={{ title: 'ハイライト再生' }} />
       </Stack.Navigator>
