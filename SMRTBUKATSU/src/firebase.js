df819c3dda01811db8e781e64b63f2e7f00008dc
inouetakehiko <inoue420@gmail.com>
2025-12-11 14:36:49 +0900

    firestoreにappIDを記載

--- SMRTBUKATSU/src/firebase.js
+++ SMRTBUKATSU/src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: 'AIzaSyDxM4skyoBwXHIUFn_bCqTfIeW3mRbtvvA',
+  authDomain: 'smart-bukatsu.firebaseapp.com',
+  projectId: 'smart-bukatsu',
+  storageBucket: 'smart-bukatsu.firebasestorage.app',
+  messagingSenderId: '217234021892',
+  appId: '1:217234021892:web:9a0313f0620b0c88e31f51',
 };
 
 const app = initializeApp(firebaseConfig);
