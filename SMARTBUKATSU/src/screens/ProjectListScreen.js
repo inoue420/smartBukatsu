88ef7b334a98dde4ce0c1fb162d316d9e21b10a1
inouetakehiko <inoue420@gmail.com>
2026-08-18 10:53:03 +0900

    ライブ動画再生可能

--- SMARTBUKATSU/src/screens/ProjectListScreen.js
+++ SMARTBUKATSU/src/screens/ProjectListScreen.js
@@ -621,7 +621,7 @@ const ProjectListScreen = ({
   const extractYoutubeId = (url) => {
     if (!url) return null;
     const regExp =
-      /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+      /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|live\/|watch\?v=|&v=)([^#&?]*).*/;
     const match = url.match(regExp);
     return match && match[2].length === 11 ? match[2] : null;
   };
